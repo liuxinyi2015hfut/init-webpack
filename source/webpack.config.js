7dc21569da1ce975ffd14e4dfe110766a26ffe5b
刘心怡 <liuxy@efacedata.com>
2019-02-22 15:59:09 +0800

    配置react常用依赖

--- source/webpack.config.js
+++ source/webpack.config.js
@@ -1,11 +1,6 @@
 let path = require('path');
-//webpack 4.0 不需要json-loader
-
 let HtmlWebpackPlugin = require('html-webpack-plugin');
-let HappyPack = require('happypack');
 
-
-//模板生成HTML文件
 let pages = [
 	{
 		title: '示例',
@@ -27,37 +22,26 @@ let pages = [
 });
 
 module.exports = {
-	// 设置webpack编译用于什么环境，默认web，可省略
 	target: 'web',
 	entry: {
 		index: './src/pages/index.js'
 	},
 	output: {
-		// //导出库的名称
 		// library:['MyLibrary','[name]' ],
-		// //配置如何暴露导出库
-		// 	//暴露为通用模块定义下都可以运行
 		// libraryTarget:'umd'
 	},
-	//bundle中排除的外部依赖
 	externals: {
-		// //import $ from 'jquery';
 		// jquery:'jQuery',
 		// echarts:'echarts'
 	},
 	module: {
-		// //不解析匹配到的模块
 		// noParse:[],
 		rules: [
-			// - 只在 test 和 文件名匹配 中使用正则表达式
-			// - 在 include 和 exclude 中使用绝对路径数组
-			// - 尽量避免 exclude，更倾向于使用 include
 			{
 				test: /\.js$/,
 				include: [path.resolve(__dirname, './src')],
 				loader: 'babel-loader',
 				options: {
-					//将转译结果缓存到文件系统中，加快babel转译速度
 					cacheDirectory: true
 				}
 			}
@@ -68,20 +52,15 @@ module.exports = {
 			// }
 		]
 	},
-	//优化配置
 	optimization: {
-		//将包含chunks映射关系的list单独提取出来，解决因为chunk的id变化导致文件缓存失效的问题
 		runtimeChunk: "single",
 
 	},
 	resolve: {
-		//使用绝对路径导入模块时，只在设定的目录中搜索
 		modules: [path.resolve(__dirname, './node_modules')],
-		// //设置导入路径别名
 		// alias: {
 		// 	'@': path.resolve(__dirname, './src')
 		// },
-		//自动解析确定的扩展，能够使用户在引入特定类型模块时不带扩展名
 		extensions: ['.js'],
 		symlinks: false
 	},
--- source/webpack.dev.js
+++ source/webpack.dev.js
@@ -7,7 +7,6 @@ let env = process.env.NODE_ENV;
 let isTest = env === 'test';
 let isDev = env === 'development';
 
-// less变量
 let lessConfig = require('./less.config');
 let openPage = `/index.html`;
 let host = 'localhost';
@@ -52,17 +51,10 @@ if (isTest) {
 
 
 module.exports = merge(commonConfig, {
-	// 选择模式告诉webpack可以相应地使用它的内置优化
-	//开发环境，启用如下
-	// NamedChunksPlugin
-	// NamedModulesPlugin。
 	mode: 'development',
 	output: {
-		// 「入口分块(entry chunk)」的文件名模板
 		filename: 'js/[name].js',
-		// 必须是绝对路径（使用 Node.js 的 path 模块）
 		path: path.resolve(__dirname, 'build'),
-		//是否在bundle中引入【所包含模块信息】的相关注释
 		// pathinfo: true
 	},
 	module: {
@@ -83,7 +75,6 @@ module.exports = merge(commonConfig, {
 					{
 						loader: 'css-loader',
 						options: {
-							//打包后调试用
 							sourceMap: true
 						}
 					},
@@ -92,9 +83,7 @@ module.exports = merge(commonConfig, {
 						loader: 'less-loader',
 						options: {
 							javascriptEnabled: true,
-							// 更换less变量
 							modifyVars: lessConfig,
-							//打包后调试用
 							sourceMap: true
 						}
 					}
@@ -119,63 +108,39 @@ module.exports = merge(commonConfig, {
 			}
 		]
 	},
-	// 用于调试
 	devtool: 'eval-source-map',
-	// // webpack-dev-server 和 webpack-dev-middleware中watch模式默认开启
 	// watch:true,
-	// 监听文件变化，排除不需要监听的文件夹
 	watchOptions: {
 		ignored: /node_modules/
 	},
 	optimization: {
-		//指定环境变量process.env.NODE_ENV，一些library根据环境变量优化代码
 		nodeEnv: env
 	},
 	plugins: [
-		//在构建之前清理构建文件夹
 		new CleanWebpackPlugin(['build'], {
-			// 不清楚的子文件
 			exclude: keepFile
 		}),
-		// 模块热替换
 		new webpack.HotModuleReplacementPlugin()
 	],
 	devServer: {
-		// 访问IP
 		host: '0.0.0.0',
-		// 监听请求的端口
 		port: port,
-		// 服务器从哪些目录中提供静态文件
 		contentBase: [
-			//开发接口假数据文件夹
 			path.resolve(__dirname, './serverJSON'),
-			//生产环境线上静态文件存放文件夹
 			path.resolve(__dirname, '../script')
 		],
-		// 启用模块热替换， 配合webpack.HotModuleReplacementPlugin使用
 		hot: true,
-		// // 启用模块热替换而无需页面刷新
 		// hotOnly:true,
-		//启用内联模式
 		inline: true,
-		// 一切服务启用gzip压缩
 		compress: true,
-		//在所有响应中添加首部
 		headers: {'Access-Control-Allow-Origin': '*'},
-		//允许浏览器使用本地IP打开网页
 		useLocalIp: true,
-		// 打开浏览器
 		open: 'Chrome',
-		//打开浏览器时的页面
 		openPage: openPage,
-		// // 启用https服务，可配置自签名证书
 		// https:true,
-		// 精确控制要显示的bundle信息
 		stats: {
-			//启用控制台的色彩输出
 			colors: true,
 		},
-		// 代理api请求
 		proxy: proxy
 	}
 });
