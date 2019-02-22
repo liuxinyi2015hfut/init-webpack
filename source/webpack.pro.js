ac19391410a3726a63421c7296ad7ed87a1e83d0
刘心怡 <liuxy@efacedata.com>
2019-02-22 17:26:06 +0800

    删除注释

--- source/webpack.pro.js
+++ source/webpack.pro.js
@@ -13,26 +13,12 @@ let domain = '/';
 let keepFile = ['JSON'];
 
 module.exports = merge(commonConfig, {
-	// 选择模式告诉webpack可以相应地使用它的内置优化
-	//生产环境，启用如下
-	// FlagDependencyUsagePlugin,
-	// FlagIncludedChunksPlugin,
-	// ModuleConcatenationPlugin,
-	// NoEmitOnErrorsPlugin,
-	// OccurrenceOrderPlugin,
-	// SideEffectsFlagPlugin
-	// UglifyJsPlugin——删除未引用代码，并压缩JS
 	mode: 'production',
 	output: {
-		// 「入口分块(entry chunk)」的文件名模板,chunkhash用于长效缓存
 		filename: 'js/[name].[chunkhash:8].js',
-		// 所有输出文件的目标路径，必须是绝对路径（使用 Node.js 的 path 模块）
 		path: path.resolve(__dirname, '../script'),
-		// 输出解析文件的目录，url 相对于 HTML 页面
 		publicPath: domain,
-		//是否在bundle中引入【所包含模块信息】的相关注释
 		pathinfo: false,
-		//非入口chunk文件名,chunkhash用于长效缓存
 		chunkFilename: 'js/[name].[chunkhash:8].js'
 	},
 	module: {
@@ -40,16 +26,13 @@ module.exports = merge(commonConfig, {
 			{
 				test: /\.(less|css)$/,
 				use: [
-					// webpack4.0 分离样式生产单独css文件
 					MiniCssExtract.loader,
 					'css-loader',
-					// 自动添加浏览器兼容性前缀
 					'postcss-loader',
 					{
 						loader: 'less-loader',
 						options: {
 							javascriptEnabled: true,
-							// 更换less变量
 							modifyVars: lessConfig,
 						}
 					}
@@ -60,7 +43,6 @@ module.exports = merge(commonConfig, {
 				include: path.resolve(__dirname, './src/common/iconfont'),
 				loader: 'url-loader',
 				options: {
-					// 8KB以内的图片转Base64
 					limit: 8192,
 					outputPath: 'iconfont/',
 					name: '[name].[hash:8].[ext]'
@@ -71,7 +53,6 @@ module.exports = merge(commonConfig, {
 				include: path.resolve(__dirname, './src/common/images'),
 				loader: 'url-loader',
 				options: {
-					// 8KB以内的图片转Base64
 					limit: 8192,
 					outputPath: 'images/',
 					name: '[name].[hash:8].[ext]'
@@ -83,25 +64,18 @@ module.exports = merge(commonConfig, {
 				loader: 'file-loader',
 				options: {
 					outputPath: 'media/',
-					// [文件名].[hash值].[格式后缀]
 					name: '[name].[hash:8].[ext]'
 				}
 			}
 		]
 	},
-	//优化配置
 	optimization: {
-		//指定环境变量process.env.NODE_ENV，一些library根据环境变量优化代码
 		nodeEnv: 'production',
 		mangleWasmImports: true,
-		//监测和删除空模块
 		removeEmptyChunks: true,
-		//监测和删除父模块中已包含的模块
 		removeAvailableModules: true,
-		//提取重复模块
 		splitChunks: {
 			cacheGroups: {
-				//项目公共组件，代码提取
 				common: {
 					chunks: 'all',
 					test: /[\\/src[\\/]/,
@@ -113,7 +87,6 @@ module.exports = merge(commonConfig, {
 					reuseExistingChunk: true
 
 				},
-				//公共第三方组件，代码提取
 				vendorCommon: {
 					chunks: 'all',
 					test: /[\\/]node_modules[\\/]/,
@@ -123,7 +96,6 @@ module.exports = merge(commonConfig, {
 					minSize: 0,
 					enforce: true
 				},
-				//独立第三方组件，代码提取
 				vendor: {
 					chunks: 'all',
 					test: /[\\/]node_modules[\\/]/,
@@ -138,18 +110,12 @@ module.exports = merge(commonConfig, {
 		}
 	},
 	plugins: [
-		//在构建之前清理构建文件夹
 		new CleanWebpackPlugin(['../script/'], {
-			//允许清楚webpack根目录之外的文件夹
 			allowExternal: true,
-			// 不清楚的子文件
 			exclude: keepFile
 		}),
-		//提取css到单独的文件
 		new MiniCssExtract({filename: 'css/[name].[contenthash:8].css'}),
-		// 压缩CSS代码
 		new OptimizeCSSAssetsPlugin(),
-		//webpack打包输出文件可视化
 		new BundleAnalyzerPlugin({
 			analyzerHost: 'localhost',
 			analyzerPort: 4321,
